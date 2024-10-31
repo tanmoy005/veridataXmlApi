ead0796b279b2d6b04f252efe32be49699a544a5
Mrinal1103 <mrinal.ghosh@elogixmail.com>
2024-10-31 11:36:04 +0530

    pwd

--- VERIDATA.Model/Request/SetNewPasswordRequest.cs
+++ VERIDATA.Model/Request/SetNewPasswordRequest.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel;
-
+using System.ComponentModel.DataAnnotations;
 namespace VERIDATA.Model.Request
 {
     public class SetNewPasswordRequest
     {
         [PasswordPropertyText]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,12}$",ErrorMessage = "Password is not in Correct format")]
+
         public string? Password { get; set; }
         public int UserId { get; set; }
         public string? clientId { get; set; }
